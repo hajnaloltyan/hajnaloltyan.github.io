c6a80cf6ec61ee160bfdd47c4bd65105d9c308d9
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2024-03-12 22:34:47 +0100

    Add and install project basics

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'fuzzy-bubbles': ['Fuzzy Bubbles', 'cursive'],
+        mali: ['Mali', 'cursive'],
+        'press-start-2p': ['Press Start 2P', 'cursive'],
+        'bungee-shade': ['Bungee Shade', 'cursive'],
+        'rampart-one': ['Rampart One', 'cursive'],
+        'rubik-moonrocks': ['Rubik Moonrocks', 'cursive'],
+      },
+      scrollBehavior: ['smooth'],
+    },
+  },
+  plugins: [],
+};
