6a319bbc559e627bb02b3614cb68e5456a6417cd
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2024-03-12 22:37:34 +0100

    Create Colours component for header

---
+++ src/components/Colours/Colours.jsx
@@ -0,0 +1,14 @@
+import './Colours.css';
+
+const Colours = () => (
+  <div className="container">
+    <div className="circle-1" />
+    <div className="circle-2" />
+    <div className="circle-3" />
+    <div className="circle-4" />
+    <div className="circle-5" />
+    <div className="circle-6" />
+  </div>
+);
+
+export default Colours;
